c2e27f43bac79178daf21adc8ad737c4074f71c3
45live <694375330@qq.com>
2024-08-02 20:49:16 +0800

    update

---
+++ src/main/java/cn/edu/ustb/wordCount/WordCount05_SlotSharingGroup.java
@@ -0,0 +1,38 @@
+package cn.edu.ustb.wordCount;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class WordCount05_SlotSharingGroup {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+
+        //全局禁用算子链
+        //env.disableOperatorChaining();
+        env.socketTextStream("localhost", 8888)
+                .flatMap((FlatMapFunction<String, String>) (s, collector) -> {
+                    for (String word : s.split(" ")) {
+                        collector.collect(word);
+                    }
+                })
+                .startNewChain()  //从当前节点开始开启一个新的链条
+                .returns(Types.STRING)
+                .map((MapFunction<String, Tuple2<String, Integer>>) value -> new Tuple2<>(value,1))
+                .slotSharingGroup("aaa") //设定共享组别为"aaa"，其他没有设定的算子默认都是属于default组
+                .returns(Types.TUPLE(Types.STRING,Types.INT))
+                .keyBy((KeySelector<Tuple2<String, Integer>, Object>) value -> value.f0)
+                .sum(1)
+                .print();
+
+        try {
+            env.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
