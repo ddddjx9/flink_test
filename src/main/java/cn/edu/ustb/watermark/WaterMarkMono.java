d1f247cfb805e14e9122c1acc629fb35d556424f
45live <694375330@qq.com>
2024-08-16 22:04:31 +0800

    update

---
+++ src/main/java/cn/edu/ustb/watermark/WaterMarkMono.java
@@ -0,0 +1,49 @@
+package cn.edu.ustb.watermark;
+
+import cn.edu.ustb.sourceOperator.WaterSensor;
+import cn.edu.ustb.transformOperator.userDefinedFunction.MyMapFunction;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+public class WaterMarkMono {
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.socketTextStream("localhost", 7777)
+                .map(new MyMapFunction())
+                .assignTimestampsAndWatermarks(WatermarkStrategy.forMonotonousTimestamps())
+                .keyBy((KeySelector<WaterSensor, String>) WaterSensor::getId)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(10L)))
+                .process(new ProcessWindowFunction<WaterSensor, String, String, TimeWindow>() {
+                    @Override
+                    public void process(String s, ProcessWindowFunction<WaterSensor, String, String, TimeWindow>.Context context, Iterable<WaterSensor> elements, Collector<String> out) throws Exception {
+                        long startTs = context.window().getStart();
+                        long endTs = context.window().getEnd();
+                        String windowStart = DateFormatUtils.format(startTs, "yyyy-MM-dd HH:mm:ss.SSS");
+                        String windowEnd = DateFormatUtils.format(endTs, "yyyy-MM-dd HH:mm:ss.SSS");
+
+                        long count = elements.spliterator().estimateSize();
+                        out.collect("key = " + s + "的窗口[" + windowStart + ", " + windowEnd + "]包含了" + count + "条数据：" + elements);
+                    }
+                })
+                .print();
+
+        //TODO 在算子外部指定Watermark策略
+        WatermarkStrategy<WaterSensor> waterSensorWatermarkStrategy = WatermarkStrategy.<WaterSensor>forMonotonousTimestamps()
+                .withTimestampAssigner(new SerializableTimestampAssigner<WaterSensor>() {
+                    @Override
+                    public long extractTimestamp(WaterSensor element, long recordTimestamp) {
+                        System.out.println(element + " ,recordTs = " + recordTimestamp);
+                        return element.getTs() * 1000L;
+                    }
+                });
+    }
+}
