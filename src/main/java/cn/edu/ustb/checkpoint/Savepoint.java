606793459c590f0810c313a237677822be230bc1
45live <694375330@qq.com>
2024-10-19 20:41:05 +0800

    update checkpoint

--- src/main/java/cn/edu/ustb/checkpoint/Savepoint.java
+++ src/main/java/cn/edu/ustb/checkpoint/Savepoint.java
@@ -1,14 +1,16 @@
 package cn.edu.ustb.checkpoint;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 public class Savepoint {
     public static void main(String[] args) {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        System.setProperty("HADOOP_USER_NAME","root");
 
-        env.socketTextStream("localhost", 8888)
+        env.socketTextStream("Hadoop132", 8888)
                 .flatMap((FlatMapFunction<String, Tuple2<String, Integer>>) (s, collector) -> {
                     String[] words = s.split(" ");
                     for (String word : words) {
@@ -18,6 +20,7 @@ public class Savepoint {
                 .uid("flatmap->wc")
                 //.name这里的参数是指显示在Web界面上让我们看到的
                 .name("wc->flatmap")
+                .returns(Types.TUPLE(Types.STRING,Types.INT))
                 .keyBy(value -> value.f0)
                 .sum(1)
                 .uid("sum->wc")
