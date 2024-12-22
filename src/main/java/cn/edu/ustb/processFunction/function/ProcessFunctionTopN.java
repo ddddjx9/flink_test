0b3e1763451c77dd53fd9e7754b32cbac6299fd2
ddddjx9 <694375330@qq.com>
2024-12-22 09:18:03 +0800

    update

--- src/main/java/cn/edu/ustb/processFunction/function/ProcessFunctionTopN.java
+++ src/main/java/cn/edu/ustb/processFunction/function/ProcessFunctionTopN.java
@@ -7,9 +7,10 @@ import org.apache.flink.util.Collector;
 import java.util.*;
 
 public class ProcessFunctionTopN extends KeyedProcessFunction<Long, Tuple3<Integer, Integer, Long>, String> {
-    //存储不同窗口的统计结果，KEY为VC，VALUE为每个窗口的排序结果
+    // 存储不同窗口的统计结果，KEY为VC，VALUE为每个窗口的排序结果
     private final Map<Long, List<Tuple3<Integer, Integer, Long>>> map;
-    private final int threshold; //通过构造器动态传参，想要展示几条结果
+    // 通过构造器动态传参，想要展示几条结果
+    private final int threshold;
 
     public ProcessFunctionTopN(int threshold) {
         this.threshold = threshold;
@@ -19,7 +20,7 @@ public class ProcessFunctionTopN extends KeyedProcessFunction<Long, Tuple3<Integ
     @Override
     public void processElement(Tuple3<Integer, Integer, Long> value, KeyedProcessFunction<Long, Tuple3<Integer, Integer, Long>, String>.Context ctx, Collector<String> out) {
 
-        //TODO 进入这个方法里面的仅为一条数据，因为process函数仅能处理一条数据，如果想要排序，得存起来一块排序
+        // TODO 进入这个方法里面的仅为一条数据，因为process函数仅能处理一条数据，如果想要排序，得存起来一块排序
         if (map.containsKey(value.f2)) {
             //如果不是第一条数据
             List<Tuple3<Integer, Integer, Long>> result = map.get(value.f2);
@@ -36,7 +37,7 @@ public class ProcessFunctionTopN extends KeyedProcessFunction<Long, Tuple3<Integ
     @Override
     public void onTimer(long timestamp, KeyedProcessFunction<Long, Tuple3<Integer, Integer, Long>, String>.OnTimerContext ctx, Collector<String> out) throws Exception {
         super.onTimer(timestamp, ctx, out);
-        //定时器触发，同一个窗口范围的计算结果攒齐了，开始排序，取TopN
+        // 定时器触发，同一个窗口范围的计算结果攒齐了，开始排序，取TopN
         Long windowEnd = ctx.getCurrentKey();
         List<Tuple3<Integer, Integer, Long>> result = map.get(windowEnd);
         result.sort(new Comparator<Tuple3<Integer, Integer, Long>>() {
@@ -46,12 +47,12 @@ public class ProcessFunctionTopN extends KeyedProcessFunction<Long, Tuple3<Integ
             }
         });
 
-        //防止因数据不够导致的数组越界问题
+        // 防止因数据不够导致的数组越界问题
         for (int i = 0; i < Math.min(threshold, result.size()); i++) {
             out.collect("当前水位线为：" + result.get(i).f0 + "，出现次数为：" + result.get(i).f1 + "，窗口结束时间为：" + result.get(i).f2);
         }
 
-        //用完的list，及时清理，节省资源
+        // 用完的list，及时清理，节省资源
         result.clear();
     }
 }
